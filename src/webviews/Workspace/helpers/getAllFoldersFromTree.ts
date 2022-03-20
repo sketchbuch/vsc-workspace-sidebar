b7332d739f08a8c7168291d23b3b4b79d6d05f56
SketchBuch <sb.design.de@gmail.com>
2022-03-20 20:42:36 +0100

    chore: add comment concerning state

--- src/webviews/Workspace/helpers/getAllFoldersFromTree.ts
+++ src/webviews/Workspace/helpers/getAllFoldersFromTree.ts
@@ -1,5 +1,6 @@
 import { FileTree } from '../helpers/getFileTree';
 
+// TODO - Put in state now that filetree is too
 export const getAllFoldersFromTree = (tree: FileTree): string[] => {
   let branch: string[] = [];
 
