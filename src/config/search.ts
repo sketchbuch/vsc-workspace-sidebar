d15bb4d6928383092f230676a0875d3883adf117
Sketchbuch <dev.sketchbuch@gmail.com>
2023-10-10 22:05:31 +0200

    chore: add search config getters

--- src/config/search.ts
+++ src/config/search.ts
@@ -1,5 +1,23 @@
 import { workspace } from 'vscode'
-import { CONFIG_SEARCH_MINIMUM } from '../constants/config'
+import {
+  CONFIG_SEARCH_CASE_INSENSITIVE,
+  CONFIG_SEARCH_MATCH_START,
+  CONFIG_SEARCH_MINIMUM,
+} from '../constants/config'
+
+export const getSearchCaseInsensitiveConfig = (): boolean => {
+  return (
+    workspace.getConfiguration().get('workspaceSidebar.search.caseInsensitive') ??
+    CONFIG_SEARCH_CASE_INSENSITIVE
+  )
+}
+
+export const getSearchMatchStartConfig = (): boolean => {
+  return (
+    workspace.getConfiguration().get('workspaceSidebar.search.matchStart') ??
+    CONFIG_SEARCH_MATCH_START
+  )
+}
 
 export const getSearchMinConfig = (): number => {
   return workspace.getConfiguration().get('workspaceSidebar.searchMinimum') ?? CONFIG_SEARCH_MINIMUM
