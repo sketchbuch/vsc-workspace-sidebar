2562023052c738b3ce694aafc4262925869bf40c
Sketchbuch <dev.sketchbuch@gmail.com>
2023-10-12 16:06:47 +0200

    chore: config cleanup and folders array added

--- src/config/folders.ts
+++ src/config/folders.ts
@@ -1,8 +1,12 @@
 import { workspace } from 'vscode'
-import { CONFIG_EXCLUDED_FOLDERS, CONFIG_FOLDER } from '../constants/config'
+import { CONFIG_EXCLUDED_FOLDERS, CONFIG_FOLDER, CONFIG_FOLDERS } from '../constants/config'
 
 export const getFolderConfig = (): string => {
   return workspace.getConfiguration().get('workspaceSidebar.folder') || CONFIG_FOLDER
+}
+
+export const getFoldersConfig = (): string[] => {
+  return workspace.getConfiguration().get('workspaceSidebar.folders') || CONFIG_FOLDERS
 }
 
 export const getExcludedFoldersConfig = (): string[] => {
--- src/constants/config.ts
+++ src/constants/config.ts
@@ -4,6 +4,7 @@ export const CONFIG_DEPTH = 0
 export const CONFIG_EXCLUDED_FOLDERS = []
 export const CONFIG_EXPLORER_COMPACT_FOLDERS = true
 export const CONFIG_FOLDER = ''
+export const CONFIG_FOLDERS = []
 export const CONFIG_SEARCH_CASE_INSENSITIVE = false
 export const CONFIG_SEARCH_MATCH_START = false
 export const CONFIG_SEARCH_MINIMUM = 15
