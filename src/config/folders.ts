5743af37564d6a1645ec48e279e4599cb12bd911
Sketchbuch <dev.sketchbuch@gmail.com>
2023-10-14 16:54:44 +0200

    chore: top exporting folder config getter

--- src/config/folders.ts
+++ src/config/folders.ts
@@ -1,7 +1,7 @@
 import { workspace } from 'vscode'
 import { CONFIG_EXCLUDED_FOLDERS, CONFIG_FOLDER, CONFIG_FOLDERS } from '../constants/config'
 
-export const getFolderConfig = (): string => {
+const getFolderConfig = (): string => {
   return workspace.getConfiguration().get('workspaceSidebar.folder') || CONFIG_FOLDER
 }
 
