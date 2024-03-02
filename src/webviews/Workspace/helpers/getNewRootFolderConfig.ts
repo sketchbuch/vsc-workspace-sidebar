7f1e6fa4e26f6af125bac3973fbdcc1cf275688c
Sketchbuch <dev.sketchbuch@gmail.com>
2024-03-02 08:44:42 +0100

    chore: remove console.log

--- src/webviews/Workspace/helpers/getNewRootFolderConfig.ts
+++ src/webviews/Workspace/helpers/getNewRootFolderConfig.ts
@@ -4,7 +4,5 @@ import { getRawFoldersConfig } from '../../../config/folders'
 import { ConfigRootFolderSettings } from '../WorkspaceViewProvider.interface'
 
 export const getNewRootFolderConfig = (workspaceFile: vscode.Uri): ConfigRootFolderSettings[] => {
-  console.log('### workspaceFile', workspaceFile)
-
   return [{ path: path.dirname(workspaceFile.fsPath) }, ...getRawFoldersConfig()]
 }
