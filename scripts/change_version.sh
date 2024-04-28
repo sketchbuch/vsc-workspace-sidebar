2ce4c9eb647fe140b7c7a4dbab1f3e87a8ec7bab
Stephen Bungert <dev.sketchbuch@gmail.com>
2024-04-28 13:21:05 +0200

    chore: update script (#168)

--- scripts/change_version.sh
+++ scripts/change_version.sh
@@ -1,6 +1,12 @@
-# ===============================================================
-# Increase version in both package.json and the Workspace WebView
-# ===============================================================
+# =========================================================
+# Increase version in both package.json and some other file
+# =========================================================
+
+# I needed to update the version in package.json and in a typescript class.
+# Version in class is used to check if caches need dropping
+# Didn't want to include package.json in the typescript class as this would have added the whole package.json to the dist bundle
+# So I created this script to update both.
+# Will exit if no version provided, but no check is made if the format of the version is correct.
 
 if [ -z $1 ]
 then
@@ -9,10 +15,10 @@ then
   exit 1
 fi
 
-FILE_PATH_WV=./src/webviews/Workspace/WorkspaceViewProvider.ts
+FILE_PATH=./src/webviews/Workspace/WorkspaceViewProvider.ts
 FILE_PATH_PKGJSON=./package.json
 
 echo "Changing version to: '$1'"
 
-sed -i "s%  private readonly _version: string.*%  private readonly _version: string = '$1'%" "$FILE_PATH_WV"
+sed -i "s%  private readonly _version: string.*%  private readonly _version: string = '$1'%" "$FILE_PATH"
 sed -i "s%  \"version\":.*%  \"version\": \"$1\"\,%" "$FILE_PATH_PKGJSON"
