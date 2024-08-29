17fcc9c26f43fd58b869263ae1b30fbaa59e10c8
sketchbuch <dev.sketchbuch@gmail.com>
2024-08-29 19:26:59 +0200

    chore: fix external workspace bug

--- src/templates/helpers/isExternalWs.ts
+++ src/templates/helpers/isExternalWs.ts
@@ -4,17 +4,16 @@ import { WorkspaceState } from '../../webviews/Workspace/WorkspaceViewProvider.i
 export const isExternalWs = (state: WorkspaceState): boolean => {
   const { rootFolders, selected, wsType } = state
 
-  return true
-
-  console.log('### isExternalWs()')
-  console.log('###  - wsType', wsType)
-  console.log('###  - selected', state.selected)
-
   if (wsType !== 'ws') {
     return false
   } else if (selected && wsType === 'ws') {
     for (let rf = 0; rf < rootFolders.length; rf++) {
       const rootFolder = rootFolders[rf]
+
+      // Could be external, but the root folder is loading so assume not external until loading finishes
+      if (selected.startsWith(rootFolder.folderPath) && rootFolder.result === 'loading') {
+        return false
+      }
 
       for (let cf = 0; cf < rootFolder.convertedFiles.length; cf++) {
         const { file } = rootFolder.convertedFiles[cf]
@@ -27,8 +26,6 @@ export const isExternalWs = (state: WorkspaceState): boolean => {
       }
     }
   }
-
-  console.log('### =========')
 
   return true
 }
